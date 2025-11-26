65c599495cfbf9d1d75421ccb7beb132afe8fec6
LuckyOx04 <ivailon27@gmail.com>
2025-11-26 15:17:34 +0200

    Initial Commit

---
+++ UserInterface/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace WeddingTableOrganizer;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
