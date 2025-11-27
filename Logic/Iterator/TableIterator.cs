1a46e249835fe4f6a40959c221810fbd4d27c39e
LuckyOx04 <ivailon27@gmail.com>
2025-11-27 13:31:51 +0200

    Iterator: Added TableIterator class.

---
+++ Logic/Iterator/TableIterator.cs
@@ -0,0 +1,25 @@
+using Logic.Composite;
+
+namespace Logic.Iterator;
+
+public class TableIterator : IIterator<Table>
+{
+    private readonly List<Table> _tables;
+    private int _position = 0;
+    
+    public TableIterator(List<Table> tables)
+    {
+        _tables = tables;
+    }
+    
+    public bool HasNext() => _position < _tables.Count;
+
+    public Table Next()
+    {
+        if (HasNext())
+        {
+            return _tables[_position++];
+        }
+        throw new IndexOutOfRangeException();
+    }
+}
